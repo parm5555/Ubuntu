180a321db9f25396130ea1e459ebd87228af59d8
parm5555 <parm5555@outlook.com>
2023-06-30 11:41:05 +0100

    a

--- provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-profile =  "admin"
-region =  "eu-west-2"
-
-
-}
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "domain-suffix" {
-  type        = string
-  description = "The DNS Suffix for the AWS Principals"
-  default = "amazonaws.com"
-}
